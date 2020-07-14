353b4c57dac590cfcf5594a2c1b192a1b4465784
Shawn_Zhou <shawn753991@hotmail.com>
2020-07-14 09:12:42 +0800

    创建工程

---
+++ src/main/java/cn/qd/shawn/mahjong/mahjongserver/MahjongServerApplication.java
@@ -0,0 +1,13 @@
+package cn.qd.shawn.mahjong.mahjongserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjongServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MahjongServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/qd/shawn/mahjong/mahjongserver/MahjongServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.qd.shawn.mahjong.mahjongserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahjongServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
